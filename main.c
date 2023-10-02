8330d1413e1ca9b1707d8ea30e08e239bd349db5
KuprikSar <i@avp1991.ru>
2023-10-02 20:21:32 +0300

    seminar2_target3_reload2

--- main.c
+++ main.c
@@ -32,10 +32,10 @@ int main(void) {
 	*/
 
 	int a, b, c;
-	float result;
 	printf("Enter numbers: \n");
 	scanf("%d%d%d", &a, &b, &c);
-	result = (a + b + c) / 3;
+	float result;
+	result = (a + b + c) / 3.f;
 	printf("resultat: %.2f\n", result);
 
 	return 0;
